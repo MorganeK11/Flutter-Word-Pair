ccd0a9ce80935d2c729a9e9f264e47e70e278082
MorganeK11 <63037684+MorganeK11@users.noreply.github.com>
2022-01-31 15:24:37 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -14,17 +14,55 @@ class MyApp extends StatelessWidget { // L'application en elle-même est un widg
 
   @override
   Widget build(BuildContext context) { // Le build décrit comment est construit le front avec ses niveaux
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter', 
-      home: Scaffold( // Le widget Scaffold contient par défaut l'appBar et le body de l'application
-        appBar: AppBar( //L'appBar contient un widget AppBar qui contient ...
-          title: const Text('Welcome to Flutter'), // ... un widget texte
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase)
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
+}
+
+
+class RandomWords extends StatefulWidget { 
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: const Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder:  (context, i) { // itemBuilder est appelé pour chaque paire de mots suggérée et les place dans une ListTile
+        if(i.isOdd) return const Divider(); // Quand la ligne est impaire, un divider est ajouté
+
+        final index = i~/2; // Renvoie l'entier de la division : ça retourne le nombre de paire de mots, moins le nombre e divider
+        if(index>= _suggestions.length) { // Si on atteint le bas de la liste,...
+          _suggestions.addAll(generateWordPairs().take(10)); //... on en génère 10 de plus et les ajoute à la liste
+        }
+        return _buildRow(_suggestions[index]); // La fonction buildRow afffiche chaque nouvelles paires dans une ListTile
+      });
+  }
+
+  Widget _buildRow(WordPair pair) {
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ),
+  );
+}
 }
